b38a294183fd6c17958580018aa2084830e1dbf7
bangbang.S <bangbang.song@gmail.com>
2014-03-08 18:58:53 +0800

    typo

--- test/uiautomator/run_test.sh
+++ test/uiautomator/run_test.sh
@@ -8,3 +8,7 @@ android create uitest-project -n $PROJECT_NAME -t $ANDROID_TARGET -p .
 ant build
 adb push bin/$PROJECT_NAME.jar /data/local/tmp/
 adb shell uiautomator runtest $PROJECT_NAME.jar -c org.bangbang.song.android.commonlib.uiautomator.DemoUiTest
+
+db shell uiautomator dump /data/local/tmp/window_dump.xml
+adb pull /data/local/tmp/window_dump.xml .
+cat window_dump.xml
