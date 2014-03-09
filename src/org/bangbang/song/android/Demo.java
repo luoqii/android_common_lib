fdd2171e93a0f155adf38e4437c86842fafc30e9
bangbang.S <bangbang.song@gmail.com>
2014-03-09 09:57:15 +0800

    add LogActivity .

--- src/org/bangbang/song/android/Demo.java
+++ src/org/bangbang/song/android/Demo.java
@@ -3,14 +3,11 @@ package org.bangbang.song.android;
 import org.bangbang.song.android.commonlib.BuildConfig;
 import org.bangbang.song.android.commonlib.ContextUtil;
 import org.bangbang.song.android.commonlib.R;
+import org.bangbang.song.android.commonlib.activity.LogActivity;
 
-import android.app.Activity;
-import android.app.KeyguardManager;
-import android.app.KeyguardManager.KeyguardLock;
-import android.content.Context;
 import android.os.Bundle;
 
-public class Demo extends Activity {
+public class Demo extends LogActivity {
     private static final String TAG = Demo.class.getSimpleName();
     // public for test only.
     public boolean mPaused;
