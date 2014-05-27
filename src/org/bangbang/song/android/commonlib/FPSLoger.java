85fef461827e41b024357ea22aa0548758a9804e
bysong <bangbangiandroid@gmail.com>
2014-05-27 16:18:48 +0800

    add onDrawBengin & onDrawEnd methods.

--- src/org/bangbang/song/android/commonlib/FPSLoger.java
+++ src/org/bangbang/song/android/commonlib/FPSLoger.java
@@ -6,6 +6,8 @@ public class FPSLoger {
         private String mTag;
         private long mStartTime;
         private int mFramecount;
+        private long mFrameStart;
+        private long mFrameEnd;
 
         public FPSLoger(String logTag) {
             mTag = logTag;
@@ -26,5 +28,15 @@ public class FPSLoger {
                     mFramecount = 0;
                 }
             }
+        }
+
+        public void onDrawStart() {
+            mFrameStart = System.currentTimeMillis();
+        }
+        public void onDrawEnd() {
+            mFrameEnd = System.currentTimeMillis();
+            
+            long elapse = mFrameEnd - mFrameStart;
+            Log.d(mTag, "frame #" + elapse);
         }
 }
